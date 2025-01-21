ba1a45ce3d443e44dbe44299e69af8c1a02463ce
arronf2e <arronf2e@163.com>
2025-01-21 13:03:26 +0000

    update user login

--- main.js
+++ main.js
@@ -1,71 +1,95 @@
-import { generateDeviceId, loadProxies, loadFile } from './utils/scripts.js';
-import { Gateway } from './utils/gateway.js';
-import log from './utils/logger.js';
-import banner from './utils/banner.js';
+import { generateDeviceId, loadProxies, loadFile } from "./utils/scripts.js";
+import { Gateway } from "./utils/gateway.js";
+import log from "./utils/logger.js";
+import banner from "./utils/banner.js";
+import { User } from "./utils/userService.js";
 
-const PROXIES_FILE = 'proxies.txt'
-const USERS_FILE = 'userIds.txt'
+const PROXIES_FILE = "proxies.txt";
+const USERS_FILE = "accounts.txt";
 const SERVER = "gw0.streamapp365.com";
 const MAX_GATEWAYS = 32;
 
 async function setupGatewaysForUser(user) {
-    const proxies = loadProxies(PROXIES_FILE);
-    const numberGateway = proxies.length > MAX_GATEWAYS ? MAX_GATEWAYS : proxies.length;
-    const userGateways = [];
-    for (let i = 0; i < numberGateway; i++) {
-        const proxy = proxies[i % proxies.length];
-        try {
-            const deviceId = generateDeviceId();
-            log.info(`Connecting to Gateway ${i + 1} for User ${user} using Device ID: ${deviceId} via Proxy: ${proxy}`);
+  const proxies = loadProxies(PROXIES_FILE);
+  const numberGateway =
+    proxies.length > MAX_GATEWAYS ? MAX_GATEWAYS : proxies.length;
+  const userGateways = [];
+  for (let i = 0; i < numberGateway; i++) {
+    const proxy = proxies[i % proxies.length];
+    try {
+      const deviceId = generateDeviceId();
+      log.info(
+        `Connecting to Gateway ${
+          i + 1
+        } for User ${user} using Device ID: ${deviceId} via Proxy: ${proxy}`
+      );
 
-            const gateway = new Gateway(SERVER, user, deviceId, proxy);
-            userGateways.push(gateway);
+      const gateway = new Gateway(SERVER, user, deviceId, proxy);
+      userGateways.push(gateway);
 
-            await new Promise((resolve) => setTimeout(resolve, 1000));
-        } catch (err) {
-            log.error(`Failed to connect Gateway ${i + 1} for User ${user}: ${err.message}`);
-        }
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    } catch (err) {
+      log.error(
+        `Failed to connect Gateway ${i + 1} for User ${user}: ${err.message}`
+      );
     }
-    return userGateways;
+  }
+  return userGateways;
+}
+
+async function getUsersUUid(users) {
+  const requests = users.map((user) => {
+    const [email, password] = user?.split("|");
+    const userInstance = new User(email, password);
+    return userInstance.login(email, password);
+  });
+  const result = await Promise.all(requests);
+  console.log(result);
 }
 
 async function main() {
-    log.info(banner);
-    const USERS = loadFile(USERS_FILE)
-    try {
-        log.info("Setting up gateways for all users...");
+  log.info(banner);
+  const USERS = loadFile(USERS_FILE);
 
-        const results = await Promise.allSettled(
-            USERS.map((user) => setupGatewaysForUser(user))
+  const userUUids = await getUsersUUid(USERS);
+
+  try {
+    log.info("Setting up gateways for all users...");
+
+    const results = await Promise.allSettled(
+      USERS.map((user) => setupGatewaysForUser(user))
+    );
+
+    results.forEach((result, index) => {
+      if (result.status === "fulfilled") {
+        log.info(
+          `User ${USERS[index]}: Successfully set up ${result.value.length} gateways.`
         );
+      } else {
+        log.error(
+          `User ${USERS[index]}: Failed to set up gateways. Reason: ${result.reason}`
+        );
+      }
+    });
 
-        results.forEach((result, index) => {
-            if (result.status === "fulfilled") {
-                log.info(`User ${USERS[index]}: Successfully set up ${result.value.length} gateways.`);
-            } else {
-                log.error(`User ${USERS[index]}: Failed to set up gateways. Reason: ${result.reason}`);
-            }
+    log.info("All user gateway setups attempted.");
+
+    process.on("SIGINT", () => {
+      log.info("Cleaning up gateways...");
+      results
+        .filter((result) => result.status === "fulfilled")
+        .flatMap((result) => result.value)
+        .forEach((gateway, index) => {
+          if (gateway.ws) {
+            log.info(`Closing Gateway ${index + 1}`);
+            gateway.ws.close();
+          }
         });
-
-        log.info("All user gateway setups attempted.");
-
-        process.on('SIGINT', () => {
-            log.info("Cleaning up gateways...");
-            results
-                .filter(result => result.status === "fulfilled")
-                .flatMap(result => result.value)
-                .forEach((gateway, index) => {
-                    if (gateway.ws) {
-                        log.info(`Closing Gateway ${index + 1}`);
-                        gateway.ws.close();
-                    }
-                });
-            process.exit();
-        });
-
-    } catch (error) {
-        log.error("Unexpected error during gateway setup:", error);
-    }
+      process.exit();
+    });
+  } catch (error) {
+    log.error("Unexpected error during gateway setup:", error);
+  }
 }
 
 // Run
---
+++ utils/userService.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+class User {
+  constructor(email, password) {
+    this.email = email;
+    this.password = password;
+    this.axios = axios.create({
+      headers: {
+        accept: "application/json",
+        "accept-language": "zh-CN,zh;q=0.9",
+        "cache-control": "no-cache",
+        "content-type": "application/json; charset=UTF-8",
+        expires: "0",
+        pragma: "no-cache",
+        priority: "u=1, i",
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"macOS"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site",
+        Referer: "https://app.allstream.ai/",
+        "Referrer-Policy": "strict-origin-when-cross-origin",
+      },
+    });
+  }
+
+  async login() {
+    const { email, password } = this;
+    const res = await this.axios.post(
+      "https://api.allstream.ai/web/v1/auth/emailLogin",
+      {
+        code: "",
+        email,
+        password,
+        referralCode: "",
+      }
+    );
+  }
+}
+
+export { User };
